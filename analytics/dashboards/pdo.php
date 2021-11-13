10042cc095d22274d4ef9a55e2cae47d202076ee
Joseph Warner <crazyforcabinets@gmail.com>
2021-11-12 22:59:55 -0500

    WillRun4Cake:   Analytics     Dashboard       Dynamically retrieve charts from the database.
          T.B.D. - Add meta data to each chart within the database.

--- analytics/dashboards/pdo.php
+++ analytics/dashboards/pdo.php
@@ -1,5 +1,83 @@
 <?php
-//  echo '{"name": "Sammy", "hair": "brown", "age": 27}';
+/*
+ *
+ *
+ *
+ *    Some of these queries may require the ONLY_FULL_GROUP_BY option to be removed from /etc/mysql/my.cnf
+ *    E.g. redeclare sql_mode:
+ *        sql_mode = "STRICT_ALL_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION"
+ *
+ *
+ *
+ */
+  header('Content-Type: application/json');
+  $conn = new PDO('mysql:host=localhost;port=3306;dbname=schoology','santa','hG8(*3te8@-)&et#uC8%3Et*7e');
+  $resultLimit = 4;
+  $query = 'SHOW FULL TABLES IN schoology WHERE TABLE_TYPE LIKE "VIEW"';
+  $stmt = $conn->prepare($query);
+  $stmt->execute();
+
+  $viewsArray = array();
+  while($result = $stmt->fetch()) {
+    array_push($viewsArray, $result[0]);
+  }
+  unset($result);
+
+  $dataArrays = array();
+  $chartArrays = array();
+  $charts = array();
+  $i = 1;
+  foreach ($viewsArray as $view) {
+    unset($stmt);
+    $data = array();
+    $view = filter_var($view, FILTER_SANITIZE_STRING);
+/*  Database names & column names cannot be bound to a prepared statement,
+    so simply use substitution in a text-based query.     */    
+    $query = "SELECT * FROM $view LIMIT $resultLimit";
+    $stmt = $conn->prepare($query);
+    $stmt->execute();
+    $j = 0;
+    while ($result = $stmt->fetch(PDO::FETCH_ASSOC)) {
+      if ($j === 0) {
+        $data[] = $result;
+      }
+      else {
+        $data[] = array_values($result);
+      }
+      $j++;
+    }
+/*  Retrieve the column names from the first record, then change
+    the record from associative to a numeric array to make JSON happy.    */
+    $colArray = array();
+    foreach (array_keys($data[0]) as $columnName) {
+      $colArray[] = $columnName;
+    }
+/*  Make numeric.   */
+    $data[0] = array_values($data[0]);
+    $dataArrays[$view] = $data;
+    $chartArrays[$view] = array();
+    $chartArrays[$view]['data'][] = $colArray;
+    foreach($data as $row) {
+      $chartArrays[$view]['data'][] = $row;
+    }
+    $chartArrays[$view]['title'] = 'DoDEA Usage by Role';
+    $chartArrays[$view]['elementId'] = 'chart'.$i;
+    $chartArrays[$view]['chartType'] = 'pie';
+    $options = array(
+      'width' => 700,
+      'height' => 500
+    );
+    $chartArrays[$view]['options'] = $options;
+    array_push($charts, $chartArrays[$view]);
+    $i++;
+  }
+  echo json_encode($charts, JSON_NUMERIC_CHECK);
+
+
+
+/*
+ *  Example static chart data.
+ */
   $chart1 = [ 'data' => 
               [
                 ['Role','Student','Teacher','School Staff','School Counselor','Educational Technologist','Above School-Level Staff','Teacher/Webmaster','School Administrator','LMS System Administrator','Instructional Designer'],
@@ -53,7 +131,7 @@
                               'height'  => 500
                            ]
            ];
-
+$comment = <<<'COMMENT'
   $charts = array();
 
   array_push($charts, $chart1);
@@ -61,4 +139,5 @@
   array_push($charts, $chart3);
 
   echo json_encode($charts);
+COMMENT;
 ?>
--- analytics/sql/common_queries.sql
+++ analytics/sql/common_queries.sql
@@ -1,6 +1,9 @@
 #  Usage Analytics Queries for Schoology (exported data)
 #  Use as guidlines for generating views.
 
+# Show all Views in the database
+SHOW FULL TABLES IN schoology WHERE TABLE_TYPE LIKE "VIEW";
+
 # Actions using External Tools (LTI)
 SELECT itemId, email, actionType, LEFT(itemName,40) AS itemName, roleName, itemType as Type, date, timestamp as Tstamp, LEFT(userBuildingName,20) FROM data GROUP BY userBuildingName, itemId, email HAVING itemType LIKE 'EXTERNAL_TOOL' AND roleName LIKE 'Student' limit 40;
 
@@ -16,14 +19,17 @@ SELECT DISTINCT(actionType) FROM data LIMIT 10;
 # All roles
 SELECT roleName as Role FROM data GROUP BY roleName LIMIT 20;
 
+# Usage by role (usage_by_role)
+SELECT roleName AS Role, COUNT(roleName) AS 'Count' FROM data GROUP BY roleName LIMIT 100;
+
 # Students that used Schoology within Date Range, by School/Building
-SELECT COUNT(email) AS '#Users', roleName AS Role, userBuildingName as Building FROM data GROUP BY Building, Role, email HAVING Role = 'Student' LIMIT 20;
+SELECT COUNT(email) AS 'Count', roleName AS 'Role', userBuildingName as 'Building' FROM data GROUP BY Building, Role, email HAVING Role = 'Student' LIMIT 20;
 
-# Student Usage by Item Type
-SELECT COUNT(itemType), itemType, roleName FROM data GROUP BY itemType HAVING itemType IN ('EXTERNAL_TOOL','PAGE','FILE','ASSIGNMENT','TEST/QUIZ','DISCUSSION', 'SCORM_PACKAGE','MEDIA_ALBUM','RESOURCE_TEST_QUIZ','RESOURCE_FOLDER') AND roleName = 'Student' LIMIT 100;
+# Student Usage by Item Type (student_usage_by_item_type)
+SELECT Type, Count FROM (SELECT COUNT(itemType) AS 'Count', itemType AS 'Type', roleName AS 'Role' FROM data GROUP BY itemType HAVING itemType IN ('EXTERNAL_TOOL','PAGE','FILE','ASSIGNMENT','TEST/QUIZ','DISCUSSION', 'SCORM_PACKAGE','MEDIA_ALBUM','RESOURCE_TEST_QUIZ','RESOURCE_FOLDER') AND roleName = 'Student' LIMIT 100) AS derived;
 
-# Top 60 Buildings/Schools by Overall Usage
-SELECT COUNT(id) AS 'Top 60 by Overall Usage', userBuildingName AS School FROM data GROUP BY userBuildingName ORDER BY COUNT(id) DESC LIMIT 50;
+# Top 3 Buildings/Schools by Overall Usage (overall_usage_by_school)
+SELECT userBuildingName AS 'School', COUNT(id) AS 'Count' FROM data GROUP BY userBuildingName ORDER BY COUNT(id) DESC LIMIT 3;
 
-# Usage by Device Type
+# Usage by Device Type (usage_by_device_type)
 SELECT COUNT(deviceType) AS 'Count', deviceType AS 'Device' FROM data GROUP BY deviceType HAVING deviceType IN ('WEB','WEB_MOBILE','ANDROID','IOS') ORDER BY Count DESC LIMIT 100;
